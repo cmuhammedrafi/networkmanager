3dbe7ea82081e08bb7722679d5c9d4333dc2951f
cmuhammedrafi <muhammed_rafi@comcast.com>
2025-05-30 12:20:02 +0000

    functional test app

--- tests/functionaltest/functional_l3_tests.cpp
+++ tests/functionaltest/functional_l3_tests.cpp
@@ -16,23 +16,57 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 **/
-#include <core/core.h>
-#include <websocket/websocket.h>
-#include <jsonrpc/jsonrpc.h>
+
+#include "Module.h"
+#include <iostream>
 
 
 using namespace WPEFramework;
 using namespace std;
 
+#define LOG_RED(fmt, ...)   printf("\033[1;31m" fmt "\033[0m\n", ##__VA_ARGS__)
+#define LOG_GREEN(fmt, ...) printf("\033[1;32m" fmt "\033[0m\n", ##__VA_ARGS__)
+
+
+std::shared_ptr<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement>> jsonPlugin;
+
+std::string getjsonResponse(std::string methodName, const JsonObject parameters = JsonObject())
+{
+    std::string result;
+    JsonObject response;
+    jsonPlugin->Invoke<JsonObject, JsonObject>(5000, _T(methodName.c_str()), parameters, response);
+    response.ToString(result);
+    if(response.HasLabel(_T("success")) && response[_T("success")].Boolean() == false) {
+        LOG_RED("Failed to get %s: %s", methodName.c_str(), response[_T("message")].String().c_str());
+        return "";
+    }
+
+    if (result.empty()) {
+        LOG_RED("No response received for %s", methodName.c_str());
+        return "";
+    }
+
+    LOG_GREEN("%s response: %s", methodName.c_str(), result.c_str());
+    return result;
+}
+
 int main ()
 {
-    sleep(60);
     Core::SystemInfo::SetEnvironment(_T("THUNDER_ACCESS"), (_T("127.0.0.1:9998")));
-
-    std::shared_ptr<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement>> jsonPlugin;
     jsonPlugin = make_shared<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement> >("org.rdk.NetworkManager.1", "");
     if (jsonPlugin == nullptr) {
         cout << "Failed to create JSONRPC link" << endl;
         return -1;
     }
+
+    getjsonResponse("GetIPSettings");
+    getjsonResponse("IsConnectedToInternet");
+    getjsonResponse("GetCaptivePortalURI");
+    getjsonResponse("GetConnectivityCheckEndpoints");
+    getjsonResponse("GetCaptivePortalURI");
+    getjsonResponse("GetPrimaryInterface");
+    getjsonResponse("GetAvailableInterfaces");
+    getjsonResponse("GetLogLevel");
+    getjsonResponse("GetStunEndpoint");
+    getjsonResponse("GetPublicIP");
 }
